40c446a08985c7b7d64153f4a055a3ff7eff3964
Gopika B <150157611+gopika30-b@users.noreply.github.com>
2025-02-26 04:17:36 +0530

    Time: 2 ms (70.83%), Space: 41.5 MB (65.13%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        for(int i=n-1; i>=0; i--){
+            if(citations[i]>=n-i) continue;
+            return n-i-1;
+        }
+        return n;
+    }
+}
